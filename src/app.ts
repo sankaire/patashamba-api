63dea0dacf713eea5d00dac1543705a259785c6d
sankaire <kerenketepela@gmail.com>
2023-08-08 14:12:23 +0300

    feat:connect to db, register api

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import db from "./config/db.js";
+import { config } from "dotenv";
+import api from "./routes/index.js";
+config();
 
 export const app = express();
-
+const connectDB = await db(process.env.DB_URI as string);
+if (!connectDB.success) {
+  console.log(connectDB.message);
+  process.exit(1);
+}
+console.log(connectDB.message);
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/v1", api);
 app.get("/api", (req: Request, res: Response) => {
   return res.status(200).json({ message: "🚀🍧✅" });
 });
