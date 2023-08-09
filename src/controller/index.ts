71611a3e9a3fb47a65ace3eb3dfd65eb59bcd106
sankaire <kerenketepela@gmail.com>
2023-08-09 13:54:41 +0300

    feat:receive data from ussd service and save it

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { ILand } from "../interface/index.js";
 import pataShambaService from "../service/index.js";
 
 const pataShamba = {
@@ -7,8 +6,27 @@ const pataShamba = {
   ussd: {
     save_data: async (req: Request, res: Response) => {
       try {
-        const data = req.body as ILand;
-        const land = await pataShambaService.ussd.save(data);
+        const data = req.body;
+        console.log(data);
+        const save_data = {
+          phone: data.phoneNumber,
+          ownerName: data.ownerName,
+          status: false,
+          land: {
+            size: data.size,
+            description: data.description,
+            fenced: data.fenced === "1" ? true : false,
+            tittled: data.tittled === "1" ? true : false,
+            price: data.charges,
+            rate: "Monthly",
+            location: {
+              county: data.county,
+              ward: data.ward,
+            },
+          },
+        } as any;
+        console.log(save_data);
+        const land = await pataShambaService.ussd.save(save_data);
         if (!land.success) {
           return res.status(400).json(data);
         }
